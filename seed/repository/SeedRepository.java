21e08fb734eb49af5d4854b83a2e5a284e3be153
d-b-tripura <52556740+d-b-tripura@users.noreply.github.com>
2021-05-26 12:56:40 +0530

    Delete seed directory

--- seed/repository/SeedRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ec.onlineplantnursery.seed.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ec.onlineplantnursery.seed.entity.Seed;
-
-public interface SeedRepository extends JpaRepository<Seed, Integer> , ISeedRepository {
-
-}
