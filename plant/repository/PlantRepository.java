6e3333c58fb073ed266286499a38d6aff0725996
d-b-tripura <52556740+d-b-tripura@users.noreply.github.com>
2021-05-26 12:56:15 +0530

    Delete plant directory

--- plant/repository/PlantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ec.onlineplantnursery.plant.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ec.onlineplantnursery.plant.entity.Plant;
-
-public interface PlantRepository extends JpaRepository<Plant, Integer> , IPlantRepository  {
-
-}
